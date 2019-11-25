a3275be950b3d93f8a36b971ccf0f289397d9fb1
Brian Metzger <bmetzger@auditor.illinois.gov>
2019-11-25 15:37:02 -0600

    Update backends.py
    Updated for Django 2.2.

--- django_auth_ldap3/backends.py
+++ django_auth_ldap3/backends.py
@@ -72,7 +72,7 @@ class LDAPBackend(object):
         # TODO: disconnect?
         pass
 
-    def authenticate(self, username=None, password=None):
+    def authenticate(self, request=None, username=None, password=None):
         """
         Required for Django auth. Authenticate the user against the LDAP
         backend and populate the local User model if this is the first
